c4c9752694ec2cdf900eb5085a4869d8d8a6abeb
Gaston ALAMACHERE <galamach@e2r2p11.42.fr>
2018-04-22 22:00:16 +0200

    Initialize repository

---
+++ index.js
@@ -0,0 +1,63 @@
+/// Reverses a string.
+// function reverse(string) {
+//   return Array.from(string).reverse().join("");
+// }
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Adds `blank` to all strings.
+String.prototype.blank = function() {
+  return this.match(/^$/);
+}
+
+// Adds `last` to all arrays.
+Array.prototype.last = function() {
+  return this.slice(-1);
+}
+
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+//  return this.processedContent() === reverse(this.processedContent());
+    return this.processedContent() === this.processedContent().reverse();
+  }
+
+    // Returns whether content is an empty string or not.
+    this.emptiness = function emptiness() {
+      return !!this.content.blank();
+    }
+
+    // Returns last character of content.
+    this.butNotLeast = function butNotLeast() {
+      Array.from(this.content).last();
+    }
+}
+
+// Defines a translated Phrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
